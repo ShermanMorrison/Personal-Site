01471380bb34047768b1ff3bde5f2d142f077159
Jonathan Wilson <jdw7@rice.edu>
2015-11-07 18:45:36 -0600

    Put canvas inside main-container

--- scripts/cube.js
+++ scripts/cube.js
@@ -1,11 +1,12 @@
 //var CUBE = (function() {
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 75, 4/3, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+var parent = $("#main-container")[0];
+renderer.setSize( parent.offsetWidth, parent.offsetHeight );
+parent.appendChild( renderer.domElement );
 
 
 
